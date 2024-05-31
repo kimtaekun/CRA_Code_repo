4a90c6a97fddb04b2a7cba7ffb53809d60ffa318
kimtaekun <cjsrnr7532@naver.com>
2024-05-31 09:08:24 +0900

    [FEAT] : matched length return 60 score

--- SimilarityChecker/Sample-Test1/SimilarityChecker_test.cpp
+++ SimilarityChecker/Sample-Test1/SimilarityChecker_test.cpp
@@ -2,6 +2,10 @@
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
 TEST(SimilarityCheckerTest, ExceptionOccurWhenInvalidChar) {
-	SimilarityChecker checker;
+	SimilarityChecker checker{ "ABC" };
 	EXPECT_THROW(checker.get_length_score("abc"), std::invalid_argument);
+}
+TEST(SimilarityCheckerTest, Get60ScoreWhenMatchedLength) {
+	SimilarityChecker checker{ "ABC" };
+	EXPECT_EQ(checker.get_length_score("ABC"), 60);
 }
--- SimilarityChecker/SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker/SimilarityChecker.cpp
@@ -5,9 +5,16 @@
 
 class SimilarityChecker {
 public:
+	SimilarityChecker(std::string _target) :target{ _target } {};
+
 	int get_length_score(std::string matched_str) {
 		if (false == std::all_of(matched_str.begin(), matched_str.end(), [](unsigned char c) {return std::isupper(c); })) {
 			throw std::invalid_argument("Must be use Upper string");
 		}
+		if (matched_str.length() == target.length()) return 60;
+		return 0;
 	}
+
+private:
+	std::string target;
 };
