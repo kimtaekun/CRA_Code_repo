20f95ab5b5c4e8f3fc58ecd34a83cc43698332c9
kimtaekun <cjsrnr7532@naver.com>
2024-05-31 09:10:46 +0900

    [REFAC] : test fixture 구현

--- SimilarityChecker/Sample-Test1/SimilarityChecker_test.cpp
+++ SimilarityChecker/Sample-Test1/SimilarityChecker_test.cpp
@@ -1,11 +1,14 @@
 #include "pch.h"
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
-TEST(SimilarityCheckerTest, ExceptionOccurWhenInvalidChar) {
+class SimilarityCheckerFixtrue : public testing::Test {
+public:
 	SimilarityChecker checker{ "ABC" };
+};
+
+TEST_F(SimilarityCheckerFixtrue, ExceptionOccurWhenInvalidChar) {
 	EXPECT_THROW(checker.get_length_score("abc"), std::invalid_argument);
 }
-TEST(SimilarityCheckerTest, Get60ScoreWhenMatchedLength) {
-	SimilarityChecker checker{ "ABC" };
+TEST_F(SimilarityCheckerFixtrue, Get60ScoreWhenMatchedLength) {
 	EXPECT_EQ(checker.get_length_score("ABC"), 60);
 }
